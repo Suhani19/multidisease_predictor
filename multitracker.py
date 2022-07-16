437d3978c119bfcffd2a8cea276e13bce042b03e
Suhani <66556476+Suhani19@users.noreply.github.com>
2022-07-16 09:53:43 +0530

    Update multitracker.py

--- multitracker.py
+++ multitracker.py
@@ -11,11 +11,11 @@ import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
 
-diabetes_model = pickle.load(open('D:/Suhani/study material/Study/Suhani/Data Science/Multipredictor/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('D:/Suhani/study material/Study/Suhani/Data Science/Multipredictor/heart_disease_model_1.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model_1.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('D:/Suhani/study material/Study/Suhani/Data Science/Multipredictor/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 with st.sidebar:
     
